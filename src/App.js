54ba586e30e971dca8087b804d5cbf3ce9ed4391
Oscar Monroy <oscarmonroy657@gmail.com>
2021-03-27 06:39:08 -0600

    CSS classes for quote component are ready.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote-container">
+        <p className="quote">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia beatae qui fuga illum recusandae maxime esse deserunt laboriosam quas distinctio reprehenderit ratione, magnam harum, aut optio obcaecati natus praesentium cupiditate!"</p>
+        <div className="quote-footer">
+          <div className="quote-footer-container">
+            <p>Bill Gates</p>
+            <span>business</span>
+          </div>
+        </div>
+        <button className="get-random-quote" type="button">Get random quote</button>
+      </div>
     </div>
   );
 }
